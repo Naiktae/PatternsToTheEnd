f91b5c3e7ec79e997b8e7f29cf1274b4227eff24
MrDydymaxx <38891111+MrDydymaxx@users.noreply.github.com>
2020-03-12 16:44:32 +0100

    YESSSS
    fonctionnalité tant attendue

--- js/story.js
+++ js/story.js
@@ -1,13 +1,11 @@
 $('#rules').append(rules);
-$("#continue").click(function(){
+$("#start").click(function(){
 	user[7] = $("#createur").val();
 	if (user[7]=="") {
 		$("#bouton").prepend('<div class="col-md-12 text-center"><p>Veuillez indiquer votre nom !</p></div>');
 	}
 	else
 	{
-		$("#bouton").empty();
-		$("#bouton").append('<div class="col-md-12 text-center"><button type="button" class="btn btn-lg btn-outline-secondary mt-4 shadow-sm" id="continue">Continuer</button></div>');
 		$("#nom").empty();
 		question(number);
 		$('#rules').empty(rules);
