fb02e6219c127d4ddbfe6aa39bfa40da74c64155
MrDydymaxx <38891111+MrDydymaxx@users.noreply.github.com>
2020-03-12 11:06:01 +0100

    script js
    base de donnée + js fresque

--- js/story.js
+++ js/story.js
@@ -1,7 +1,15 @@
 $('#rules').append(rules);
 $("#continue").click(function(){
 	user[7] = $("#createur").val();
-	$("#nom").empty();
-	question(number);
-	$('#rules').empty(rules);
+	if (user[7]=="") {
+		$("#bouton").prepend('<div class="col-md-12 text-center"><p>Veuillez indiquer votre nom !</p></div>');
+	}
+	else
+	{
+		$("#bouton").empty();
+		$("#bouton").append('<div class="col-md-12 text-center"><button type="button" class="btn btn-lg btn-outline-secondary mt-4 shadow-sm" id="continue">Continuer</button></div>');
+		$("#nom").empty();
+		question(number);
+		$('#rules').empty(rules);
+	}
 });
